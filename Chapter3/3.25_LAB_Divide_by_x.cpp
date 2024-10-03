b046e4ba7b1e628aa941213a348dc65222ba9b7d
bweatherly-bw <brandonweatherly58@gmail.com>
2024-10-03 16:05:02 -0500

    C++ Code for the zybook labs for chapters 2 and 3

---
+++ Chapter3/3.25_LAB_Divide_by_x.cpp
@@ -0,0 +1,31 @@
+// 3.25 LAB: Divide by x
+
+// Write a program using integers userNum and x as input, and output userNum divided by x three times.
+
+// Ex: If the input is:
+
+// 2000 2
+// the output is:
+
+// 1000 500 250
+// Note: In C++, integer division discards fractions. Ex: 6 / 4 is 1 (the 0.5 is discarded).
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+   int userNum;
+   int x;
+   
+   cin >> userNum;
+   cin >> x;
+   
+   cout << userNum / x << " ";
+   cout << userNum / (x*x) << " ";
+   cout << userNum / (x*x*x) << endl;
+
+   return 0;
+}
---
+++ Chapter3/3.26_LAB_Expression for calories burned_during_workout.cpp
@@ -0,0 +1,39 @@
+// 3.26 LAB: Expression for calories burned during workout
+
+// The following equation estimates the average calories burned for a person when exercising, which is based on a scientific journal article (source):
+
+// Calories = ( (Age x 0.2757) + (Weight x 0.03295) + (Heart Rate x 1.0781) — 75.4991 ) x Time / 8.368
+
+// Write a program using inputs age (years), weight (pounds), heart rate (beats per minute), and time (minutes), respectively. Output the average calories burned for a person.
+
+// Output each floating-point value with two digits after the decimal point, which can be achieved by executing
+// cout << fixed << setprecision(2); once before all other cout statements.
+
+// Ex: If the input is:
+
+// 49 155 148 60
+// the output is:
+
+// Calories: 736.21 calories
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   
+   int age;
+   int weight;
+   int heart;
+   int tTime;
+   
+   cin >> age >> weight >> heart >> tTime;
+   
+   double calories = ((age * 0.2757) + (weight * 0.03295) + (heart * 1.0781) - 75.4991 ) * tTime / 8.368;
+   
+   cout << fixed << setprecision(2);
+   cout << "Calories: " << calories << " calories" << endl;
+
+   return 0;
+}
